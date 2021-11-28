6792155be932690763120aaf7f8f13659c3e5840
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-11-28 22:00:29 +0500

    tech task 1 works

---
+++ App/Buhgaltery/Buhgaltery.DbClient/SettingsEntityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Buhgaltery.DbClient
+{
+    public class SettingsEntityConfiguration : IEntityTypeConfiguration<Settings>
+    {
+        public void Configure(EntityTypeBuilder<Settings> builder)
+        {            
+            builder.ToTable("settings");
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.ParamName).HasColumnName("param_name");
+            builder.Property(s => s.ParamValue).HasColumnName("param_value");
+        }
+    }
+}
