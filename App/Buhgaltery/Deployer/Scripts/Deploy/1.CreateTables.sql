ab59e2478326898b5a5c44b843602971b31512f6
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2021-12-29 09:41:26 +0500

    tech task 0.0.1 works

--- App/Buhgaltery/Deployer/Scripts/Deploy/1.CreateTables.sql
+++ App/Buhgaltery/Deployer/Scripts/Deploy/1.CreateTables.sql
@@ -42,3 +42,20 @@ create table if not exists product(
 	, version_date  timestamptz   not null
 	, is_deleted    boolean       not null default false	
 );
+
+create table if not exists h_product(
+	  h_id          bigserial     not null primary key        
+    , id            uuid          null
+	, "name"        varchar(100)  null
+	, "description" varchar(1000) null
+	, parent_id     uuid          null
+	, add_period    int           null
+	, min_value     int           null
+	, max_value     int           null
+	, is_leaf       boolean       null
+	, last_add_date timestamptz   null
+	, version_date  timestamptz   null
+	, is_deleted    boolean       null
+	, change_date   timestamptz   not null default now()
+	, "user_id"     varchar       null
+);
