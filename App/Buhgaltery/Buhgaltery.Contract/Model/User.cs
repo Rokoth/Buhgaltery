e5c18f04d96aa0401f22d87841268535038f5ed0
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2022-01-28 15:21:37 +0500

    tech task 0.0.1 works

--- App/Buhgaltery/Buhgaltery.Contract/Model/User.cs
+++ App/Buhgaltery/Buhgaltery.Contract/Model/User.cs
@@ -22,10 +22,15 @@ namespace Buhgaltery.Contract.Model
         public string Login { get; set; }
         [Display(Name = "Email")]
         public string Email { get; set; }
+        [Display(Name = "Только листовые элементы")]
         public bool LeafOnly { get; set; }
+        [Display(Name = "ИД формулы")]
         public Guid FormulaId { get; set; }
+        [Display(Name = "Формула")]
         public string Formula { get; set; }
+        [Display(Name = "Дата последнего добавления элемента в резервы")]
         public DateTimeOffset? LastAddedDate { get; set; }
+        [Display(Name = "Период добавления элементов в резервы")]
         public int AddPeriod { get; set; }
     }
 }
